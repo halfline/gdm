27d2bab34effc6f035522c82920e6f76b01fccd3
Ray Strode <rstrode@redhat.com>
2015-02-02 16:11:28 -0500

    daemon: move plymouth code from slave to manager
    In an effort to pare down the slave code, this commit
    moves the plymouth bits to GdmManager.

--- daemon/gdm-simple-slave.c
+++ daemon/gdm-simple-slave.c
@@ -64,10 +64,6 @@ struct GdmSimpleSlavePrivate
         guint              connection_attempts;
 
         GdmServer         *server;
-
-#ifdef  WITH_PLYMOUTH
-        guint              plymouth_is_running : 1;
-#endif
 };
 
 enum {
@@ -79,83 +75,9 @@ static void     gdm_simple_slave_init           (GdmSimpleSlave      *simple_sla
 
 G_DEFINE_TYPE (GdmSimpleSlave, gdm_simple_slave, GDM_TYPE_SLAVE)
 
-#ifdef  WITH_PLYMOUTH
-static gboolean
-plymouth_is_running (void)
-{
-        int      status;
-        gboolean res;
-        GError  *error;
-
-        error = NULL;
-        res = g_spawn_command_line_sync ("/bin/plymouth --ping",
-                                         NULL, NULL, &status, &error);
-        if (! res) {
-                g_debug ("Could not ping plymouth: %s", error->message);
-                g_error_free (error);
-                return FALSE;
-        }
-
-        return WIFEXITED (status) && WEXITSTATUS (status) == 0;
-}
-
-static void
-plymouth_prepare_for_transition (GdmSimpleSlave *slave)
-{
-        gboolean res;
-        GError  *error;
-
-        error = NULL;
-        res = g_spawn_command_line_sync ("/bin/plymouth deactivate",
-                                         NULL, NULL, NULL, &error);
-        if (! res) {
-                g_warning ("Could not deactivate plymouth: %s", error->message);
-                g_error_free (error);
-        }
-}
-
-static void
-plymouth_quit_with_transition (GdmSimpleSlave *slave)
-{
-        gboolean res;
-        GError  *error;
-
-        error = NULL;
-        res = g_spawn_command_line_sync ("/bin/plymouth quit --retain-splash",
-                                         NULL, NULL, NULL, &error);
-        if (! res) {
-                g_warning ("Could not quit plymouth: %s", error->message);
-                g_error_free (error);
-        }
-        slave->priv->plymouth_is_running = FALSE;
-}
-
-static void
-plymouth_quit_without_transition (GdmSimpleSlave *slave)
-{
-        gboolean res;
-        GError  *error;
-
-        error = NULL;
-        res = g_spawn_command_line_sync ("/bin/plymouth quit",
-                                         NULL, NULL, NULL, &error);
-        if (! res) {
-                g_warning ("Could not quit plymouth: %s", error->message);
-                g_error_free (error);
-        }
-        slave->priv->plymouth_is_running = FALSE;
-}
-#endif
-
 static void
 setup_server (GdmSimpleSlave *slave)
 {
-#ifdef WITH_PLYMOUTH
-        /* Plymouth is waiting for the go-ahead to exit */
-        if (slave->priv->plymouth_is_running) {
-                plymouth_quit_with_transition (slave);
-        }
-#endif
 }
 
 static gboolean
@@ -194,12 +116,6 @@ on_server_exited (GdmServer      *server,
         g_debug ("GdmSimpleSlave: server exited with code %d\n", exit_code);
 
         gdm_slave_stop (GDM_SLAVE (slave));
-
-#ifdef WITH_PLYMOUTH
-        if (slave->priv->plymouth_is_running) {
-                plymouth_quit_without_transition (slave);
-        }
-#endif
 }
 
 static void
@@ -212,12 +128,6 @@ on_server_died (GdmServer      *server,
                  g_strsignal (signal_number));
 
         gdm_slave_stop (GDM_SLAVE (slave));
-
-#ifdef WITH_PLYMOUTH
-        if (slave->priv->plymouth_is_running) {
-                plymouth_quit_without_transition (slave);
-        }
-#endif
 }
 
 static gboolean
@@ -265,13 +175,6 @@ gdm_simple_slave_run (GdmSimpleSlave *slave)
                                   G_CALLBACK (on_server_ready),
                                   slave);
 
-#ifdef WITH_PLYMOUTH
-                slave->priv->plymouth_is_running = plymouth_is_running ();
-
-                if (slave->priv->plymouth_is_running) {
-                        plymouth_prepare_for_transition (slave);
-                }
-#endif
                 res = gdm_server_start (slave->priv->server);
                 if (! res) {
                         g_warning (_("Could not start the X "
@@ -282,11 +185,6 @@ gdm_simple_slave_run (GdmSimpleSlave *slave)
                                      "In the meantime this display will be "
                                      "disabled.  Please restart GDM when "
                                      "the problem is corrected."));
-#ifdef WITH_PLYMOUTH
-                        if (slave->priv->plymouth_is_running) {
-                                plymouth_quit_without_transition (slave);
-                        }
-#endif
                         exit (1);
                 }
 
