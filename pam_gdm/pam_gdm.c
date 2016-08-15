429fcfc86b065e2ed99c83f97d174b2f5a307a14
Ray Strode <rstrode@redhat.com>
2016-08-15 14:11:01 -0400

    pam: grab cached password from systemd and pass it on
    If the user has an encrypted disk then systemd will cache the password
    they type into the keyring. It makes sense to try to use this password
    for automatic login purposes first, since on single user machines,
    the sole user password is likely to match the disk password.
    
    Of course if it doesn't work we'll fall back to the old way of doing
    automatic login without a password (and then the user will have to
    manualy enter if they need to for gnome-keyring or whatever)
    
    https://bugzilla.gnome.org/show_bug.cgi?id=769950

--- pam_gdm/pam_gdm.c
+++ pam_gdm/pam_gdm.c
@@ -17,11 +17,15 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
+#include <unistd.h>
+
 #include <security/_pam_macros.h>
 #include <security/pam_ext.h>
 #include <security/pam_misc.h>
 #include <security/pam_modules.h>
 #include <security/pam_modutil.h>
+
+#include <keyutils.h>
 
 int
 pam_sm_authenticate (pam_handle_t  *pamh,
@@ -29,6 +33,27 @@ pam_sm_authenticate (pam_handle_t  *pamh,
                      int            argc,
                      const char   **argv)
 {
+        int r;
+        void *cached_password = NULL;
+        key_serial_t serial;
+
+        serial = find_key_by_type_and_desc ("user", "cryptsetup", 0);
+        if (serial == 0)
+                return PAM_AUTHINFO_UNAVAIL;
+
+        r = keyctl_read_alloc (serial, &cached_password);
+        if (r < 0)
+                return PAM_AUTHINFO_UNAVAIL;
+
+        sleep (20);
+
+        r = pam_set_item (pamh, PAM_AUTHTOK, cached_password);
+
+        free (cached_password);
+
+        if (r < 0)
+                return PAM_AUTH_ERR;
+
         return PAM_SUCCESS;
 }
 
