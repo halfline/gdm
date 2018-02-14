0012497171500090b55e4c8e967809a5b2daaa94
Ray Strode <rstrode@redhat.com>
2018-02-14 16:07:15 -0500

    Revert "session: don't call gdm_session_defaults_changed from setup"
    This reverts commit 572a19324b75cc1f1b2db4908e2d7c9f06e4e335.
    
    It turns out we need this call for more than just the session type,
    we also need to it to inform the greeter about the default session
    to use.

--- daemon/gdm-session.c
+++ daemon/gdm-session.c
@@ -2395,6 +2395,7 @@ gdm_session_setup (GdmSession *self,
         update_session_type (self);
 
         initialize (self, service_name, NULL, NULL);
+        gdm_session_defaults_changed (self);
 }
 
 
@@ -2413,6 +2414,7 @@ gdm_session_setup_for_user (GdmSession *self,
 
         self->priv->is_program_session = FALSE;
         initialize (self, service_name, self->priv->selected_user, NULL);
+        gdm_session_defaults_changed (self);
 }
 
 void
